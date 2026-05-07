d8bb206649366afb64102d0b9ad11ea106e945d4
parit888 <jvposifachim@gmail.com>
2026-05-07 07:56:58 -0300

    Add files via upload

---
+++ portaria/script.js
@@ -0,0 +1,5 @@
+function checkAge() {
+    var wasborn = document.getElementById("birth").value;
+    var res = document.getElementById("result");
+    var clientAge = document's.getElementById("age");
+}
